ad971866d6c29d0967394640fa598a340cf4ffc7
Haraldo Filho <haraldo.filho@gmail.com>
2020-11-12 16:22:43 -0300

    Changed sidepanel's map style and added markers to it

---
+++ pages/nos2viajando/airports.js
@@ -0,0 +1,16 @@
+var airports = [
+  [[-47.137552, -23.008121], 'Aeroporto de Viracopos (VCP)'],
+  [[-51.175306, -29.993566], 'Aeroporto de Porto Alegre (POA)'],
+  [[-54.488620, -25.598144], 'Aeroporto de Foz do Iguaçu (IGU)'],
+  [[-46.473064, -23.429726], 'Aeroporto de Guarulhos (GRU)'],
+  [[-74.174473, 40.689515], 'Aeroporto de Newark (EWR)'],
+  [[-122.379020, 37.621432], 'Aeroporto de São Francisco (SFO)'],
+  [[-95.336579, 29.990220], 'Aeroporto de Houston (IAH)'],
+  [[-34.923919, -8.125910], 'Aeroporto de Recife (REC)'],
+  [[-32.424450, -3.853822], 'Aeroporto de Fernando de Noronha (FEN)'],
+  [[12.246163, 41.799919], 'Aeroporto de Roma (FCO)'],
+  [[16.298966, 43.537061], 'Aeroporto de Split (SPU)'],
+  [[16.067458, 45.740863], 'Aeroporto de Zagrebe (ZAG)'],
+  [[18.262164, 42.560850], 'Aeroporto de Dubrovnik (DBV)'],
+  [[14.289361, 40.884847], 'Aeroporto de Nápoles (NAP)']
+]
