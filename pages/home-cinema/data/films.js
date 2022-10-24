cf2b637e9e67dbce9c251fb4de16abfa1d8613a3
Haraldo Filho <haraldo.filho@gmail.com>
2022-10-24 17:11:06 -0300

    Changed watched films list on home-cinema page from static to dynamic

---
+++ pages/home-cinema/data/films.js
@@ -0,0 +1,12 @@
+var films = [
+  ['https://letterboxd.com/hpfilho/film/free-guy/', 'https://a.ltrbxd.com/resized/film-poster/4/7/9/8/1/4/479814-free-guy-0-600-0-900-crop.jpg?v=0cd7303445'],
+  ['https://letterboxd.com/hpfilho/film/the-last-duel-2021/', 'https://a.ltrbxd.com/resized/film-poster/5/4/4/3/8/0/544380-the-last-duel-0-600-0-900-crop.jpg?v=b3cf1ae7a1'],
+  ['https://letterboxd.com/hpfilho/film/upstream-color', 'https://a.ltrbxd.com/resized/sm/upload/gp/ky/a2/x2/ckbUVwa9r7h348upwiRvjpl4sQp-0-600-0-900-crop.jpg?v=c08832ee85'],
+  ['https://letterboxd.com/hpfilho/film/drive-my-car/', 'https://a.ltrbxd.com/resized/film-poster/6/7/9/2/9/1/679291-drive-my-car-0-600-0-900-crop.jpg?v=9e1f7c9f35'],
+  ['https://letterboxd.com/hpfilho/film/maelstrom/', 'https://a.ltrbxd.com/resized/film-poster/2/8/5/1/2/28512-maelstrom-0-600-0-900-crop.jpg?v=bbbd2f69ec'],
+  ['https://letterboxd.com/hpfilho/film/crimes-of-the-future-2022/', 'https://a.ltrbxd.com/resized/film-poster/7/3/6/3/1/8/736318-crimes-of-the-future-0-600-0-900-crop.jpg?v=ae8a4b1603'],
+  ['https://letterboxd.com/hpfilho/film/the-worlds-end/', 'https://a.ltrbxd.com/resized/film-poster/8/6/9/9/1/86991-the-world-s-end-0-600-0-900-crop.jpg?v=48a7252f0d'],
+  ['https://letterboxd.com/hpfilho/film/everything-everywhere-all-at-once/', 'https://a.ltrbxd.com/resized/film-poster/4/7/4/4/7/4/474474-everything-everywhere-all-at-once-0-600-0-900-crop.jpg?v=281f1a041e'],
+  ['https://letterboxd.com/hpfilho/film/top-gun/', 'https://a.ltrbxd.com/resized/sm/upload/e9/dh/nn/5w/fKSZl8ONN7DQh5ujgdObRIFmhiZ-0-600-0-900-crop.jpg?v=042d149015'],
+  ['https://letterboxd.com/hpfilho/film/prey-2022/', 'https://a.ltrbxd.com/resized/film-poster/6/8/6/3/8/9/686389-prey-0-600-0-900-crop.jpg?v=f9f0c6bb6e']
+]
