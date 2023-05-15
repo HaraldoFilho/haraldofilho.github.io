afb36eec7dbf4caf0f2bfa25e34cb368faadb853
HaraldoFilho <haraldo.filho@gmail.com>
2023-05-15 11:25:30 -0300

    [auto] Updated home-cinema page

--- pages/home-cinema/data/collection.js
+++ pages/home-cinema/data/collection.js
@@ -1,9 +1,9 @@
 var collection = [
-  ['Films', 318, 'https://letterboxd.com/hpfilho/list/my-film-collection/', 'collection'],
+  ['Films', 320, 'https://letterboxd.com/hpfilho/list/my-film-collection/', 'collection'],
   ['Miniseries', 3, 'https://letterboxd.com/hpfilho/list/my-miniseries-collection/', 'collection'],
-  ['Blu-ray', 111, 'https://letterboxd.com/hpfilho/list/blu-ray/', 'disc'],
+  ['Blu-ray', 114, 'https://letterboxd.com/hpfilho/list/blu-ray/', 'disc'],
   ['DVD', 263, 'https://letterboxd.com/hpfilho/list/dvd/', 'disc'],
-  ['BD/DVD', 53, 'https://letterboxd.com/hpfilho/list/blu-ray-dvd/', 'disc'],
-  ['Dolby Atmos', 15, 'https://letterboxd.com/hpfilho/list/dolby-atmos/', 'audio'],
+  ['BD/DVD', 54, 'https://letterboxd.com/hpfilho/list/blu-ray-dvd/', 'disc'],
+  ['Dolby Atmos', 16, 'https://letterboxd.com/hpfilho/list/dolby-atmos/', 'audio'],
   ['DTS X', 6, 'https://letterboxd.com/hpfilho/list/dts-x/', 'audio'],
 ]
