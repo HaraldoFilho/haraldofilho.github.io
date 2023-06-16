a0122e893bd25ee87a9c1ca619546bb04c878821
HaraldoFilho <haraldo.filho@gmail.com>
2023-06-15 23:59:58 -0300

    [auto] Updated home-cinema page

--- pages/home-cinema/data/collection.js
+++ pages/home-cinema/data/collection.js
@@ -1,7 +1,7 @@
 var collection = [
-  ['Films', 326, 'https://letterboxd.com/hpfilho/list/my-film-collection/', 'collection'],
+  ['Films', 327, 'https://letterboxd.com/hpfilho/list/my-film-collection/', 'collection'],
   ['Miniseries', 4, 'https://letterboxd.com/hpfilho/list/my-miniseries-collection/', 'collection'],
-  ['Blu-ray', 122, 'https://letterboxd.com/hpfilho/list/my-blu-ray-discs/', 'disc'],
+  ['Blu-ray', 123, 'https://letterboxd.com/hpfilho/list/my-blu-ray-discs/', 'disc'],
   ['DVD', 264, 'https://letterboxd.com/hpfilho/list/my-dvds/', 'disc'],
   ['BD/DVD', 56, 'https://letterboxd.com/hpfilho/list/blu-ray-dvd/', 'disc'],
   ['Dolby Atmos', 21, 'https://letterboxd.com/hpfilho/list/my-dolby-atmos-discs/', 'audio'],
