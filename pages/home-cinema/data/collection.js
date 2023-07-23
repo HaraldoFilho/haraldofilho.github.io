d0af33ca4d48c39c07d6c74ab0175cb975f73bc3
HaraldoFilho <haraldo.filho@gmail.com>
2023-07-23 00:00:32 -0300

    [auto] Updated home-cinema page

--- pages/home-cinema/data/collection.js
+++ pages/home-cinema/data/collection.js
@@ -1,11 +1,11 @@
 var collection = [
-  ['Movies', 396, 'https://letterboxd.com/hpfilho/list/my-home-cinema-shelf/', 'shelf'],
-  ['Films', 330, 'https://letterboxd.com/hpfilho/list/my-film-collection/', 'collection'],
+  ['Movies', 398, 'https://letterboxd.com/hpfilho/list/my-home-cinema-shelf/', 'shelf'],
+  ['Films', 332, 'https://letterboxd.com/hpfilho/list/my-film-collection/', 'collection'],
   ['Shorts', 68, 'https://letterboxd.com/hpfilho/list/my-short-films-collection/', 'collection'],
   ['Miniseries', 4, 'https://letterboxd.com/hpfilho/list/my-miniseries-collection/', 'collection'],
-  ['Blu-ray', 129, 'https://letterboxd.com/hpfilho/list/my-blu-ray-discs/', 'disc'],
+  ['Blu-ray', 131, 'https://letterboxd.com/hpfilho/list/my-blu-ray-discs/', 'disc'],
   ['DVD', 263, 'https://letterboxd.com/hpfilho/list/my-dvds/', 'disc'],
   ['BD/DVD', 58, 'https://letterboxd.com/hpfilho/list/blu-ray-dvd/', 'disc'],
-  ['Dolby Atmos', 23, 'https://letterboxd.com/hpfilho/list/my-dolby-atmos-discs/', 'audio'],
+  ['Dolby Atmos', 24, 'https://letterboxd.com/hpfilho/list/my-dolby-atmos-discs/', 'audio'],
   ['DTS X', 8, 'https://letterboxd.com/hpfilho/list/my-dts-x-discs/', 'audio'],
 ]
