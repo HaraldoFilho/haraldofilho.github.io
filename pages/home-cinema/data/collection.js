e9a539b612eb6625a954d414243973097067a044
HaraldoFilho <haraldo.filho@gmail.com>
2023-08-08 00:00:05 -0300

    [auto] Updated home-cinema page

--- pages/home-cinema/data/collection.js
+++ pages/home-cinema/data/collection.js
@@ -4,9 +4,9 @@ var collection = [
   ['Shorts', 69, 'https://letterboxd.com/hpfilho/list/my-short-films-collection/', 'collection'],
   ['Documentaries', 19, 'https://letterboxd.com/hpfilho/list/my-special-feature-documentaries-collection/', 'collection'],
   ['Miniseries', 4, 'https://letterboxd.com/hpfilho/list/my-miniseries-collection/', 'collection'],
-  ['Blu-ray', 131, 'https://letterboxd.com/hpfilho/list/my-blu-ray-discs/', 'disc'],
+  ['Blu-ray', 132, 'https://letterboxd.com/hpfilho/list/my-blu-ray-discs/', 'disc'],
   ['DVD', 263, 'https://letterboxd.com/hpfilho/list/my-dvds/', 'disc'],
-  ['BD/DVD', 58, 'https://letterboxd.com/hpfilho/list/blu-ray-dvd/', 'disc'],
+  ['BD/DVD', 59, 'https://letterboxd.com/hpfilho/list/blu-ray-dvd/', 'disc'],
   ['Dolby Atmos', 24, 'https://letterboxd.com/hpfilho/list/my-dolby-atmos-discs/', 'audio'],
   ['DTS X', 8, 'https://letterboxd.com/hpfilho/list/my-dts-x-discs/', 'audio'],
 ]
