87306f546e7660ed392019e51e52ea21d78aeac2
HaraldoFilho <haraldo.filho@gmail.com>
2023-09-23 21:00:38 -0300

    [auto] Updated home-cinema page

--- pages/home-cinema/data/films.js
+++ pages/home-cinema/data/films.js
@@ -1,4 +1,5 @@
 var films = [
+  ['https://letterboxd.com/hpfilho/film/no-one-will-save-you/', 'https://a.ltrbxd.com/resized/film-poster/7/3/6/8/2/1/736821-no-one-will-save-you-0-600-0-900-crop.jpg', '★'],
   ['https://letterboxd.com/hpfilho/film/john-wick-chapter-3-parabellum/', 'https://a.ltrbxd.com/resized/film-poster/3/9/0/0/3/9/390039-john-wick-chapter-3-parabellum-0-600-0-900-crop.jpg', '★★★★'],
   ['https://letterboxd.com/hpfilho/film/fall-2022/', 'https://a.ltrbxd.com/resized/film-poster/8/8/7/6/2/1/887621-fall-0-600-0-900-crop.jpg', '★★½'],
   ['https://letterboxd.com/hpfilho/film/the-descent/', 'https://a.ltrbxd.com/resized/film-poster/4/7/2/0/4/47204-the-descent-0-600-0-900-crop.jpg', '★★★★'],
@@ -18,5 +19,4 @@ var films = [
   ['https://letterboxd.com/hpfilho/film/mission-impossible-rogue-nation/', 'https://a.ltrbxd.com/resized/sm/upload/lk/jb/17/6y/oUDKymEL932AeHyl4A4ee8zuBLF-0-600-0-900-crop.jpg', '★★★★'],
   ['https://letterboxd.com/hpfilho/film/the-wonder/', 'https://a.ltrbxd.com/resized/film-poster/7/4/0/1/6/2/740162-the-wonder-0-600-0-900-crop.jpg', '★★★½'],
   ['https://letterboxd.com/hpfilho/film/last-night-in-soho/', 'https://a.ltrbxd.com/resized/film-poster/5/0/5/0/0/0/505000-last-night-in-soho-0-600-0-900-crop.jpg', '★★★★'],
-  ['https://letterboxd.com/hpfilho/film/synchronic/', 'https://a.ltrbxd.com/resized/film-poster/4/7/8/1/0/7/478107-synchronic-0-600-0-900-crop.jpg', '★★★'],
 ]
