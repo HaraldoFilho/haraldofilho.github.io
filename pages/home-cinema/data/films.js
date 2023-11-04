3c611b54ab53b63586b81c794b743d438f9e3d6a
HaraldoFilho <haraldo.filho@gmail.com>
2023-11-03 21:00:37 -0300

    [auto] Updated home-cinema page

--- pages/home-cinema/data/films.js
+++ pages/home-cinema/data/films.js
@@ -1,4 +1,5 @@
 var films = [
+  ['John Wick: Chapter 4, 2023', 'https://letterboxd.com/hpfilho/film/john-wick-chapter-4/', 'https://a.ltrbxd.com/resized/film-poster/5/3/0/8/8/2/530882-john-wick-chapter-4-0-600-0-900-crop.jpg', '★★★★'],
   ['A Haunting in Venice, 2023', 'https://letterboxd.com/hpfilho/film/a-haunting-in-venice/', 'https://a.ltrbxd.com/resized/film-poster/8/5/0/3/7/5/850375-a-haunting-in-venice-0-600-0-900-crop.jpg', '★★★½'],
   ['Fresh, 2022', 'https://letterboxd.com/hpfilho/film/fresh-2022/', 'https://a.ltrbxd.com/resized/film-poster/7/0/6/0/6/4/706064-fresh-0-600-0-900-crop.jpg', '★★★'],
   ['Nightmare Alley, 2021', 'https://letterboxd.com/hpfilho/film/nightmare-alley-2021/', 'https://a.ltrbxd.com/resized/film-poster/5/2/4/5/9/2/524592-nightmare-alley-0-600-0-900-crop.jpg', '★★★'],
