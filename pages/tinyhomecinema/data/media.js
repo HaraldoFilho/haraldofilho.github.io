3b5fc7766cfbc93a977e2b45a0f616745d464ba5
HaraldoFilho <haraldo.filho@gmail.com>
2023-11-10 21:00:42 -0300

    [auto] Updated tinyhomecinema page

--- pages/tinyhomecinema/data/media.js
+++ pages/tinyhomecinema/data/media.js
@@ -2,7 +2,7 @@ var shelves = [
   ['Movies', 400, 'https://letterboxd.com/hpfilho/list/my-home-cinema-shelf/', 'shelf'],
   ['TV Shows', 6, 'https://www.themoviedb.org/list/8256420-my-tv-shows-home-cinema-shelf', 'shelf'],
   ['Music', 35, 'https://tinyhomecinema.page/music/', 'shelf'],
-  ['Books', 26, 'https://www.librarything.com/catalog.php?view=hpfilho&collection=794026&shelf=shelf&sort=title&previousOffset=0&shelf_rows=3&collection=794026', 'shelf']
+  ['Books', 27, 'https://www.librarything.com/catalog.php?view=hpfilho&collection=794026&shelf=shelf&sort=title&previousOffset=0&shelf_rows=3&collection=794026', 'shelf']
 ]
 
 var collection = [
