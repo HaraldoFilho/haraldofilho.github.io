8bdbe92c70b2d9f2be409fd258d30073c359729b
HaraldoFilho <haraldo.filho@gmail.com>
2023-12-11 17:01:21 -0300

    [auto] Updated tinyhomecinema page

--- pages/tinyhomecinema/data/films.js
+++ pages/tinyhomecinema/data/films.js
@@ -1,12 +1,12 @@
 var films = [
-  ['Leave the World Behind', '2023', 'https://letterboxd.com/hpfilho/film/leave-the-world-behind-2023/', 'https://a.ltrbxd.com/resized/film-poster/6/4/8/8/6/9/648869-leave-the-world-behind-0-600-0-900-crop.jpg', '★★★★'],
+  ['Leave the World Behind', '2023', 'https://letterboxd.com/hpfilho/film/leave-the-world-behind-2023/', 'https://a.ltrbxd.com/resized/film-poster/6/4/8/8/6/9/648869-leave-the-world-behind-0-600-0-900-crop.jpg', '★★★★½'],
   ['Aftersun', '2022', 'https://letterboxd.com/hpfilho/film/aftersun/', 'https://a.ltrbxd.com/resized/film-poster/8/6/8/5/5/8/868558-aftersun-0-600-0-900-crop.jpg', '★★★½'],
   ['The Killer', '2023', 'https://letterboxd.com/hpfilho/film/the-killer-2023/', 'https://a.ltrbxd.com/resized/film-poster/7/1/7/6/5/4/717654-the-killer-0-600-0-900-crop.jpg', '★★★½'],
   ['John Wick: Chapter 4', '2023', 'https://letterboxd.com/hpfilho/film/john-wick-chapter-4/', 'https://a.ltrbxd.com/resized/film-poster/5/3/0/8/8/2/530882-john-wick-chapter-4-0-600-0-900-crop.jpg', '★★★★'],
   ['A Haunting in Venice', '2023', 'https://letterboxd.com/hpfilho/film/a-haunting-in-venice/', 'https://a.ltrbxd.com/resized/film-poster/8/5/0/3/7/5/850375-a-haunting-in-venice-0-600-0-900-crop.jpg', '★★★½'],
   ['Fresh', '2022', 'https://letterboxd.com/hpfilho/film/fresh-2022/', 'https://a.ltrbxd.com/resized/film-poster/7/0/6/0/6/4/706064-fresh-0-600-0-900-crop.jpg', '★★★'],
-  ['No Hard Feelings', '2023', 'https://letterboxd.com/hpfilho/film/no-hard-feelings-2023/', 'https://a.ltrbxd.com/resized/film-poster/7/9/5/8/9/7/795897-no-hard-feelings-0-600-0-900-crop.jpg', '★★★'],
-  ['No One Will Save You', '2023', 'https://letterboxd.com/hpfilho/film/no-one-will-save-you/', 'https://a.ltrbxd.com/resized/film-poster/7/3/6/8/2/1/736821-no-one-will-save-you-0-600-0-900-crop.jpg', '★'],
-  ['Fall', '2022', 'https://letterboxd.com/hpfilho/film/fall-2022/', 'https://a.ltrbxd.com/resized/film-poster/8/8/7/6/2/1/887621-fall-0-600-0-900-crop.jpg', '★★½'],
-  ['Holy Spider', '2022', 'https://letterboxd.com/hpfilho/film/holy-spider/', 'https://a.ltrbxd.com/resized/film-poster/8/0/0/8/4/8/800848-holy-spider-0-600-0-900-crop.jpg', '★★★½'],
+  ['Leave the World Behind', '2023', 'https://letterboxd.com/hpfilho/film/leave-the-world-behind-2023/', 'https://a.ltrbxd.com/resized/film-poster/6/4/8/8/6/9/648869-leave-the-world-behind-0-600-0-900-crop.jpg', '★★★★'],
+  ['Aftersun', '2022', 'https://letterboxd.com/hpfilho/film/aftersun/', 'https://a.ltrbxd.com/resized/film-poster/8/6/8/5/5/8/868558-aftersun-0-600-0-900-crop.jpg', '★★★½'],
+  ['The Killer', '2023', 'https://letterboxd.com/hpfilho/film/the-killer-2023/', 'https://a.ltrbxd.com/resized/film-poster/7/1/7/6/5/4/717654-the-killer-0-600-0-900-crop.jpg', '★★★½'],
+  ['John Wick: Chapter 4', '2023', 'https://letterboxd.com/hpfilho/film/john-wick-chapter-4/', 'https://a.ltrbxd.com/resized/film-poster/5/3/0/8/8/2/530882-john-wick-chapter-4-0-600-0-900-crop.jpg', '★★★★'],
 ]
--- pages/tinyhomecinema/data/services.js
+++ pages/tinyhomecinema/data/services.js
@@ -5,7 +5,7 @@ var services = [
   ['Apple TV+', 'https://letterboxd.com/hpfilho/tag/cineminha/films/on/apple-tv-plus-br/', 'current'],
   ['STAR+', 'https://letterboxd.com/hpfilho/tag/cineminha/films/on/starplus-br/', 'current'],
   ['Disney+', 'https://letterboxd.com/hpfilho/tag/cineminha/films/on/disney-plus-br/', 'current'],
-  ['Lionsgate+', 'https://letterboxd.com/hpfilho/tag/cineminha/films/on/lionsgateplus-br/', 'current'],
+  ['Lionsgate+', 'https://letterboxd.com/hpfilho/tag/cineminha/films/on/lionsgateplus-br/', 'past'],
   ['Paramount+', 'https://letterboxd.com/hpfilho/tag/cineminha/films/on/paramountplus-br/', 'past'],
   ['MUBI', 'https://letterboxd.com/hpfilho/tag/cineminha/films/on/mubi-br/', 'current'],
   ['Looke', 'https://letterboxd.com/hpfilho/tag/cineminha/films/on/looke-br/', ''],
@@ -14,5 +14,5 @@ var services = [
   ['MGM', 'https://letterboxd.com/hpfilho/tag/cineminha/films/on/mgm-br/', ''],
   ['Belas Artes', 'https://letterboxd.com/hpfilho/tag/cineminha/films/on/belasartesalacarte-br/', 'current'],
   ['Oldflix', 'https://letterboxd.com/hpfilho/tag/cineminha/films/on/oldflix-br/', ''],
-  ['Filmicca', '', '']
+  ['Filmicca', 'https://letterboxd.com/hpfilho/tag/cineminha/films/on/filmicca-br/', '']
 ]
