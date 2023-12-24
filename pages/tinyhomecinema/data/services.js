272b8730991c73596e0a476f29ca468bad90b558
HaraldoFilho <haraldo.filho@gmail.com>
2023-12-24 17:00:50 -0300

    [auto] Updated tinyhomecinema page

--- pages/tinyhomecinema/data/services.js
+++ pages/tinyhomecinema/data/services.js
@@ -5,14 +5,14 @@ var services = [
   ['Apple TV+', 'https://letterboxd.com/tinyhomecinema/tag/apple-tv-/films/', 'current'],
   ['STAR+', 'https://letterboxd.com/tinyhomecinema/tag/star-/films/', 'current'],
   ['Disney+', 'https://letterboxd.com/tinyhomecinema/tag/disney-/films/', 'current'],
-  ['Paramount+', '', 'past'],
+  ['Paramount+', 'https://letterboxd.com/tinyhomecinema/tag/paramount-/films/', 'past'],
   ['Lionsgate+', 'https://letterboxd.com/tinyhomecinema/tag/lionsgate-/films/', 'past'],
-  ['MUBI', 'https://letterboxd.com/tinyhomecinema/tag/mubi/films/', 'current'],
+  ['MUBI', 'https://letterboxd.com/tinyhomecinema/tag/mubi/films/', 'past'],
   ['Looke', 'https://letterboxd.com/tinyhomecinema/tag/looke/films/', ''],
   ['Telecine Play', 'https://letterboxd.com/tinyhomecinema/tag/telecine-play/films/', ''],
   ['Globoplay', 'https://letterboxd.com/tinyhomecinema/tag/globoplay/films/', ''],
   ['MGM', 'https://letterboxd.com/tinyhomecinema/tag/mgm/films/', ''],
   ['Belas Artes', 'https://letterboxd.com/tinyhomecinema/tag/belas-artes/films/', 'current'],
   ['Oldflix', 'https://letterboxd.com/tinyhomecinema/tag/oldflix/films/', ''],
-  ['Filmicca', 'https://letterboxd.com/tinyhomecinema/tag/filmicca/films/', '']
+  ['Filmicca', 'https://letterboxd.com/tinyhomecinema/tag/filmicca/films/', 'current']
 ]
