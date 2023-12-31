0462aae516877a417967f04135455ec8b54fd365
HaraldoFilho <haraldo.filho@gmail.com>
2023-12-30 23:00:44 -0300

    [auto] Updated tinyhomecinema page

--- pages/tinyhomecinema/data/films.js
+++ pages/tinyhomecinema/data/films.js
@@ -1,4 +1,5 @@
 var films = [
+  ['Are You There God? It\'s Me, Margaret.', '2023', 'https://letterboxd.com/tinyhomecinema/film/are-you-there-god-its-me-margaret/', 'https://a.ltrbxd.com/resized/film-poster/4/8/3/9/5/1/483951-are-you-there-god-it-s-me-margaret--0-600-0-900-crop.jpg'],
   ['Leave the World Behind', '2023', 'https://letterboxd.com/tinyhomecinema/film/leave-the-world-behind-2023/', 'https://a.ltrbxd.com/resized/film-poster/6/4/8/8/6/9/648869-leave-the-world-behind-0-600-0-900-crop.jpg'],
   ['Aftersun', '2022', 'https://letterboxd.com/tinyhomecinema/film/aftersun/', 'https://a.ltrbxd.com/resized/film-poster/8/6/8/5/5/8/868558-aftersun-0-600-0-900-crop.jpg'],
   ['The Killer', '2023', 'https://letterboxd.com/tinyhomecinema/film/the-killer-2023/', 'https://a.ltrbxd.com/resized/film-poster/7/1/7/6/5/4/717654-the-killer-0-600-0-900-crop.jpg'],
@@ -8,5 +9,4 @@ var films = [
   ['No Hard Feelings', '2023', 'https://letterboxd.com/tinyhomecinema/film/no-hard-feelings-2023/', 'https://a.ltrbxd.com/resized/film-poster/7/9/5/8/9/7/795897-no-hard-feelings-0-600-0-900-crop.jpg'],
   ['No One Will Save You', '2023', 'https://letterboxd.com/tinyhomecinema/film/no-one-will-save-you/', 'https://a.ltrbxd.com/resized/film-poster/7/3/6/8/2/1/736821-no-one-will-save-you-0-600-0-900-crop.jpg'],
   ['Fall', '2022', 'https://letterboxd.com/tinyhomecinema/film/fall-2022/', 'https://a.ltrbxd.com/resized/film-poster/8/8/7/6/2/1/887621-fall-0-600-0-900-crop.jpg'],
-  ['Holy Spider', '2022', 'https://letterboxd.com/tinyhomecinema/film/holy-spider/', 'https://a.ltrbxd.com/resized/film-poster/8/0/0/8/4/8/800848-holy-spider-0-600-0-900-crop.jpg'],
 ]
