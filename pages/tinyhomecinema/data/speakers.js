48c14c2d8a171ba0a2823ddba220d5310856e06d
HaraldoFilho <haraldo.filho@gmail.com>
2024-01-11 11:31:54 -0300

    [auto] Updated tinyhomecinema page

--- pages/tinyhomecinema/data/speakers.js
+++
@@ -1,7 +0,0 @@
-var speakers = [
-  ['FL/FR', 'Yamaha NS-B51', 'https://europe.yamaha.com/en/products/audio_visual/speaker_systems/ns-p51/'],
-  ['C', 'Yamaha NS-C51', 'https://europe.yamaha.com/en/products/audio_visual/speaker_systems/ns-p51/'],
-  ['Sub', 'Yamaha NS-SW050', 'https://europe.yamaha.com/en/products/audio_visual/speaker_systems/ns-sw050/'],
-  ['SL/SR', 'Dayton Audio SAT-BK', 'https://www.daytonaudio.com/product/757/sat-bk-2-way-satellite-speaker-pair-black/'],
-  ['HL/HR', 'Dayton Audio SAT-BK', 'https://www.daytonaudio.com/product/757/sat-bk-2-way-satellite-speaker-pair-black/']
-]
