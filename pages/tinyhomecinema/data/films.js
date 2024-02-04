a328fb44ec4dc60fb05c23159e0df89155448d24
HaraldoFilho <haraldo.filho@gmail.com>
2024-02-04 19:00:51 -0300

    [auto] Updated tinyhomecinema page

--- pages/tinyhomecinema/data/films.js
+++ pages/tinyhomecinema/data/films.js
@@ -1,4 +1,5 @@
 var films = [
+  ['Dumb Money', '2023', 'https://letterboxd.com/tinyhomecinema/film/dumb-money/', 'https://a.ltrbxd.com/resized/film-poster/7/1/0/0/7/3/710073-dumb-money-0-600-0-900-crop.jpg'],
   ['Talk to Me', '2022', 'https://letterboxd.com/tinyhomecinema/film/talk-to-me-2022/', 'https://a.ltrbxd.com/resized/film-poster/9/0/8/1/4/4/908144-talk-to-me-0-600-0-900-crop.jpg'],
   ['Gran Turismo', '2023', 'https://letterboxd.com/tinyhomecinema/film/gran-turismo/', 'https://a.ltrbxd.com/resized/film-poster/8/8/2/7/1/8/882718-gran-turismo-0-600-0-900-crop.jpg'],
   ['Are You There God? It\'s Me, Margaret.', '2023', 'https://letterboxd.com/tinyhomecinema/film/are-you-there-god-its-me-margaret/', 'https://a.ltrbxd.com/resized/film-poster/4/8/3/9/5/1/483951-are-you-there-god-it-s-me-margaret--0-600-0-900-crop.jpg'],
@@ -8,5 +9,4 @@ var films = [
   ['John Wick: Chapter 4', '2023', 'https://letterboxd.com/tinyhomecinema/film/john-wick-chapter-4/', 'https://a.ltrbxd.com/resized/film-poster/5/3/0/8/8/2/530882-john-wick-chapter-4-0-600-0-900-crop.jpg'],
   ['A Haunting in Venice', '2023', 'https://letterboxd.com/tinyhomecinema/film/a-haunting-in-venice/', 'https://a.ltrbxd.com/resized/film-poster/8/5/0/3/7/5/850375-a-haunting-in-venice-0-600-0-900-crop.jpg'],
   ['Fresh', '2022', 'https://letterboxd.com/tinyhomecinema/film/fresh-2022/', 'https://a.ltrbxd.com/resized/film-poster/7/0/6/0/6/4/706064-fresh-0-600-0-900-crop.jpg'],
-  ['No Hard Feelings', '2023', 'https://letterboxd.com/tinyhomecinema/film/no-hard-feelings-2023/', 'https://a.ltrbxd.com/resized/film-poster/7/9/5/8/9/7/795897-no-hard-feelings-0-600-0-900-crop.jpg'],
 ]
