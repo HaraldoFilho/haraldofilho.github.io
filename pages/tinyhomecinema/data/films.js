e21b16cc508bf6f7166ad4eb81e1e7c718e07bdd
HaraldoFilho <haraldo.filho@gmail.com>
2024-02-10 23:00:38 -0300

    [auto] Updated tinyhomecinema page

--- pages/tinyhomecinema/data/films.js
+++ pages/tinyhomecinema/data/films.js
@@ -1,4 +1,5 @@
 var films = [
+  ['The Hijacking of Flight 375', '2023', 'https://letterboxd.com/tinyhomecinema/film/the-hijacking-of-flight-375/', 'https://a.ltrbxd.com/resized/film-poster/9/3/1/1/4/2/931142-the-hijacking-of-flight-375-0-600-0-900-crop.jpg'],
   ['Society of the Snow', '2023', 'https://letterboxd.com/tinyhomecinema/film/society-of-the-snow/', 'https://a.ltrbxd.com/resized/film-poster/8/1/3/8/4/0/813840-society-of-the-snow-0-600-0-900-crop.jpg'],
   ['The Abandoned', '2022', 'https://letterboxd.com/tinyhomecinema/film/the-abandoned-2022/', 'https://a.ltrbxd.com/resized/film-poster/9/0/7/0/2/6/907026-the-abandoned-0-600-0-900-crop.jpg'],
   ['Dumb Money', '2023', 'https://letterboxd.com/tinyhomecinema/film/dumb-money/', 'https://a.ltrbxd.com/resized/film-poster/7/1/0/0/7/3/710073-dumb-money-0-600-0-900-crop.jpg'],
@@ -8,5 +9,4 @@ var films = [
   ['Leave the World Behind', '2023', 'https://letterboxd.com/tinyhomecinema/film/leave-the-world-behind-2023/', 'https://a.ltrbxd.com/resized/film-poster/6/4/8/8/6/9/648869-leave-the-world-behind-0-600-0-900-crop.jpg'],
   ['Aftersun', '2022', 'https://letterboxd.com/tinyhomecinema/film/aftersun/', 'https://a.ltrbxd.com/resized/film-poster/8/6/8/5/5/8/868558-aftersun-0-600-0-900-crop.jpg'],
   ['The Killer', '2023', 'https://letterboxd.com/tinyhomecinema/film/the-killer-2023/', 'https://a.ltrbxd.com/resized/film-poster/7/1/7/6/5/4/717654-the-killer-0-600-0-900-crop.jpg'],
-  ['John Wick: Chapter 4', '2023', 'https://letterboxd.com/tinyhomecinema/film/john-wick-chapter-4/', 'https://a.ltrbxd.com/resized/film-poster/5/3/0/8/8/2/530882-john-wick-chapter-4-0-600-0-900-crop.jpg'],
 ]
