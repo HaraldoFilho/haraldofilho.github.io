8197e222f23ffcb1cdb988e2716e7bffc9299af5
HaraldoFilho <haraldo.filho@gmail.com>
2024-02-27 17:00:46 -0300

    [auto] Updated tinyhomecinema page

--- pages/tinyhomecinema/data/services.js
+++ pages/tinyhomecinema/data/services.js
@@ -1,9 +1,9 @@
 var services = [
   ['Netflix', 'https://letterboxd.com/tinyhomecinema/tag/netflix/films/', 'current'],
   ['Prime Video', 'https://letterboxd.com/tinyhomecinema/tag/prime-video/films/', 'current'],
-  ['HBO Max', 'https://letterboxd.com/tinyhomecinema/tag/hbo-max/films/', 'current'],
   ['Apple TV+', 'https://letterboxd.com/tinyhomecinema/tag/apple-tv-/films/', 'current'],
-  ['Globoplay', 'https://letterboxd.com/tinyhomecinema/tag/globoplay/films/', ''],
+  ['Max', 'https://letterboxd.com/tinyhomecinema/tag/max/films/', 'current'],
+  ['Globoplay', 'https://letterboxd.com/tinyhomecinema/tag/globoplay/films/', 'current'],
   ['STAR+', 'https://letterboxd.com/tinyhomecinema/tag/star-/films/', 'past'],
   ['Disney+', 'https://letterboxd.com/tinyhomecinema/tag/disney-/films/', 'past'],
   ['Filmicca', 'https://letterboxd.com/tinyhomecinema/tag/filmicca/films/', 'current'],
@@ -14,5 +14,6 @@ var services = [
   ['Oldflix', 'https://letterboxd.com/tinyhomecinema/tag/oldflix/films/', ''],
   ['Telecine Play', 'https://letterboxd.com/tinyhomecinema/tag/telecine-play/films/', ''],
   ['MGM', 'https://letterboxd.com/tinyhomecinema/tag/mgm/films/', ''],
+  ['HBO Max', 'https://letterboxd.com/tinyhomecinema/tag/hbo-max/films/', 'closed'],
   ['Lionsgate+', 'https://letterboxd.com/tinyhomecinema/tag/lionsgate-/films/', 'closed'],
 ]
