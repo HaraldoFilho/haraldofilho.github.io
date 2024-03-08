2247a03e49997b204179750fa25012eaf0f6bf3e
HaraldoFilho <haraldo.filho@gmail.com>
2024-03-08 17:00:49 -0300

    [auto] Updated tinyhomecinema page

--- pages/tinyhomecinema/data/media.js
+++ pages/tinyhomecinema/data/media.js
@@ -6,11 +6,11 @@ var shelves = [
 ]
 
 var collection = [
-  ['Films', 340, 'https://letterboxd.com/tinyhomecinema/list/film-collection/', 'collection'],
+  ['Films', 341, 'https://letterboxd.com/tinyhomecinema/list/film-collection/', 'collection'],
   ['Miniseries', 2, 'https://letterboxd.com/tinyhomecinema/list/miniseries/', 'collection'],
   ['Shorts', 68, 'https://letterboxd.com/tinyhomecinema/list/short-films/', 'collection'],
   ['Documentaries', 19, 'https://letterboxd.com/tinyhomecinema/list/special-feature-documentaries/', 'collection'],
-  ['Blu-ray', 141, 'https://letterboxd.com/tinyhomecinema/list/blu-ray/', 'media'],
+  ['Blu-ray', 142, 'https://letterboxd.com/tinyhomecinema/list/blu-ray/', 'media'],
   ['DVD', 239, 'https://letterboxd.com/tinyhomecinema/list/dvd/', 'media'],
   ['VHS', 5, 'https://letterboxd.com/tinyhomecinema/list/decor-vhs-tapes/', 'media'],
   ['Digital', 4, 'https://letterboxd.com/tinyhomecinema/list/digital/', 'media'],
