87c26720d49f7c464f069230a4f48bb320c1d02b
HaraldoFilho <haraldo.filho@gmail.com>
2024-03-12 10:04:51 -0300

    [auto] Updated tinyhomecinema page

--- pages/tinyhomecinema/data/media.js
+++ pages/tinyhomecinema/data/media.js
@@ -6,14 +6,14 @@ var shelves = [
 ]
 
 var collection = [
-  ['Films', 341, 'https://letterboxd.com/tinyhomecinema/list/film-collection/', 'collection'],
+  ['Films', 343, 'https://letterboxd.com/tinyhomecinema/list/film-collection/', 'collection'],
   ['Miniseries', 2, 'https://letterboxd.com/tinyhomecinema/list/miniseries/', 'collection'],
   ['Shorts', 68, 'https://letterboxd.com/tinyhomecinema/list/short-films/', 'collection'],
   ['Documentaries', 19, 'https://letterboxd.com/tinyhomecinema/list/special-feature-documentaries/', 'collection'],
   ['Blu-ray', 142, 'https://letterboxd.com/tinyhomecinema/list/blu-ray/', 'media'],
   ['DVD', 239, 'https://letterboxd.com/tinyhomecinema/list/dvd/', 'media'],
   ['VHS', 5, 'https://letterboxd.com/tinyhomecinema/list/decor-vhs-tapes/', 'media'],
-  ['Digital', 4, 'https://letterboxd.com/tinyhomecinema/list/digital/', 'media'],
+  ['Digital', 6, 'https://letterboxd.com/tinyhomecinema/list/digital/', 'media'],
   ['Dolby Atmos', 25, 'https://letterboxd.com/tinyhomecinema/list/dolby-atmos/', 'audio'],
   ['DTS X', 8, 'https://letterboxd.com/tinyhomecinema/list/dts-x/', 'audio'],
 ]
