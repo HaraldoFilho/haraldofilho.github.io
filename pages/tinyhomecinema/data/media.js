666ee1ee4c4291289cefb41714df5a84850e5048
HaraldoFilho <haraldo.filho@gmail.com>
2024-03-14 13:25:19 -0300

    [auto] Updated tinyhomecinema page

--- pages/tinyhomecinema/data/media.js
+++ pages/tinyhomecinema/data/media.js
@@ -1,5 +1,5 @@
 var shelves = [
-  ['Movies', 404, 'https://letterboxd.com/tinyhomecinema/list/movies-shelf/', 'shelf'],
+  ['Movies', 406, 'https://letterboxd.com/tinyhomecinema/list/movies-shelf/', 'shelf'],
   ['TV Shows', 7, 'https://www.themoviedb.org/list/8283859-tv-shows-shelf?view=grid', 'shelf'],
   ['Music', 35, 'https://tinyhomecinema.page/music/', 'shelf'],
   ['Books', 27, 'https://www.librarything.com/catalog.php?view=hpfilho&collection=794026&shelf=shelf&sort=title&previousOffset=0&shelf_rows=3&collection=794026', 'shelf']
@@ -11,7 +11,7 @@ var collection = [
   ['Shorts', 68, 'https://letterboxd.com/tinyhomecinema/list/short-films/', 'collection'],
   ['Documentaries', 19, 'https://letterboxd.com/tinyhomecinema/list/special-feature-documentaries/', 'collection'],
   ['Blu-ray', 142, 'https://letterboxd.com/tinyhomecinema/list/blu-ray/', 'media'],
-  ['DVD', 240, 'https://letterboxd.com/tinyhomecinema/list/dvd/', 'media'],
+  ['DVD', 242, 'https://letterboxd.com/tinyhomecinema/list/dvd/', 'media'],
   ['VHS', 5, 'https://letterboxd.com/tinyhomecinema/list/decor-vhs-tapes/', 'media'],
   ['Digital', 6, 'https://letterboxd.com/tinyhomecinema/list/digital/', 'media'],
   ['Dolby Atmos', 25, 'https://letterboxd.com/tinyhomecinema/list/dolby-atmos/', 'audio'],
